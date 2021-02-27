57942f51a8b115945df932a304f0cd261bbd7695
DashaMarinkevich <dasha_marinkevich@mail.ru>
2021-02-27 20:12:00 +0300

    feat: add new code to index.js and package

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const numWords = require('num-words');
+    return numWords(number).replace(' and', '');
 }
